5d783ed15b2d06007f4300b0f1bf96e667ee81bf
lisa <lizlicha1117@gmail.com>
2024-05-10 11:52:03 +0900

    デバッグ用にテストデータ復活ボタンを追加

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -29,6 +29,32 @@ const TodoList = () => {
         .catch(error => console.error('Error updating todo:', error));
     };
 
+    const handleReset = () => {
+        const ids = [1, 2, 3];
+        const resetPromises = ids.map(id =>
+            fetch(`https://56n12ow66c.execute-api.ap-northeast-1.amazonaws.com/Prod/todos`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id, completed: false })
+            })
+        );
+
+        Promise.all(resetPromises)
+            .then(responses => {
+                if (responses.every(response => response.ok)) {
+                    fetch('https://56n12ow66c.execute-api.ap-northeast-1.amazonaws.com/Prod/todos')
+                        .then(response => response.json())
+                        .then(data => setTodos(data))
+                        .catch(error => console.error('Error fetching todos:', error));
+                } else {
+                    console.error('Failed to reset todos');
+                }
+            })
+            .catch(error => console.error('Error resetting todos:', error));
+    };
+
     const formatTags = (tag1, tag2, tag3) => {
         const tags = [tag1, tag2, tag3].filter(tag => tag).map(tag => `#${tag}`);
         return tags.join(' ');
@@ -37,6 +63,7 @@ const TodoList = () => {
     return (
         <div className="todo-list">
             <h1>Todo List</h1>
+            <button onClick={handleReset}>テストデータ復活(デバッグ用)</button>
             <ul>
                 {todos.map(todo => (
                     <li key={todo.id}>
