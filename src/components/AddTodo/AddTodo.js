15ca70d4d3c9ba79e980f67349024d7ff5cb0a29
butta00 <takuhei1445@icloud.com>
2024-05-22 19:23:31 +0900

    日付と時間をうちこめるようにしたよ

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,16 +1,13 @@
-// AddTodo.js
 import React, { useState } from 'react';
 import './AddTodo.css';
 
-// Enterキーが押されたときにフォームが送信されないようにする関数
 const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-        event.preventDefault();
+        event.preventDefault();  // Enterキーでのフォーム送信を防止
     }
 };
 
 const AddTodo = ({ onAddTodo }) => {
-    // フォームの入力データを管理するステートを定義
     const [newTodo, setNewTodo] = useState({
         user_name: '',
         title: '',
@@ -18,10 +15,11 @@ const AddTodo = ({ onAddTodo }) => {
         tag1: '',
         tag2: '',
         tag3: '',
+        date: '',  // 日付のステート
+        time: '',  // 時間のステート
         completed: false,
     });
 
-    // 入力フィールドの値が変更されたときに呼び出される関数
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setNewTodo({
@@ -30,11 +28,8 @@ const AddTodo = ({ onAddTodo }) => {
         });
     };
 
-    // フォームが送信されたときに呼び出される関数
     const handleAddTodo = (event) => {
-        event.preventDefault(); // フォームのデフォルトの送信動作を防ぐ
-
-        // 新しいTodo項目をAPIに送信する関数
+        event.preventDefault();
         fetch('https://56n12ow66c.execute-api.ap-northeast-1.amazonaws.com/Prod/todos', {
             method: 'POST',
             headers: {
@@ -44,12 +39,9 @@ const AddTodo = ({ onAddTodo }) => {
         })
         .then(response => response.json())
         .then(data => {
-            console.log('Todo added:', data); // 成功した場合のログ
-            onAddTodo(newTodo); // 親コンポーネントに新しいTodoを追加する
+            onAddTodo(newTodo);
         })
         .catch(error => console.error('Error adding todo:', error));
-
-        // フォームをリセットする
         setNewTodo({
             user_name: '',
             title: '',
@@ -57,61 +49,26 @@ const AddTodo = ({ onAddTodo }) => {
             tag1: '',
             tag2: '',
             tag3: '',
+            date: '',
+            time: '',
             completed: false,
         });
     };
 
-    // フォームのレンダリング
     return (
         <form onSubmit={handleAddTodo}>
-            <input
-                type="text"
-                name="user_name"
-                placeholder="ユーザー名"
-                value={newTodo.user_name}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown} // Enterキーが押されたときの動作を制御
-            />
-            <input
-                type="text"
-                name="title"
-                placeholder="タイトル"
-                value={newTodo.title}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown} // Enterキーが押されたときの動作を制御
-            />
-            <input
-                type="text"
-                name="description"
-                placeholder="詳細"
-                value={newTodo.description}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown} // Enterキーが押されたときの動作を制御
-            />
-            <input
-                type="text"
-                name="tag1"
-                placeholder="タグ1"
-                value={newTodo.tag1}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown} // Enterキーが押されたときの動作を制御
-            />
-            <input
-                type="text"
-                name="tag2"
-                placeholder="タグ2"
-                value={newTodo.tag2}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown} // Enterキーが押されたときの動作を制御
-            />
-            <input
-                type="text"
-                name="tag3"
-                placeholder="タグ3"
-                value={newTodo.tag3}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown} // Enterキーが押されたときの動作を制御
-            />
+            <input type="text" name="user_name" placeholder="ユーザー名" value={newTodo.user_name} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+            <input type="text" name="title" placeholder="タイトル" value={newTodo.title} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+            <div className="datetime-container">
+                <input type="date" name="date" value={newTodo.date} onChange={handleInputChange} className="datetime-input" />
+                <input type="time" name="time" value={newTodo.time} onChange={handleInputChange} className="datetime-input" />
+            </div>
+            <input type="text" name="description" placeholder="詳細" value={newTodo.description} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+            <div className="tag-container">
+                <input type="text" name="tag1" placeholder="タグ1" value={newTodo.tag1} onChange={handleInputChange} onKeyDown={handleKeyDown} className="tag-input" />
+                <input type="text" name="tag2" placeholder="タグ2" value={newTodo.tag2} onChange={handleInputChange} onKeyDown={handleKeyDown} className="tag-input" />
+                <input type="text" name="tag3" placeholder="タグ3" value={newTodo.tag3} onChange={handleInputChange} onKeyDown={handleKeyDown} className="tag-input" />
+            </div>
             <button type="submit">追加</button>
         </form>
     );
