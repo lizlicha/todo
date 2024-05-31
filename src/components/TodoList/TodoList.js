bba57a940b28cba9502b5e90a8d42a3a1395d638
butta00 <takuhei1445@icloud.com>
2024-05-31 10:17:55 +0900

    とりあえずセーブ

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -54,12 +54,12 @@ const TodoList = () => {
     return (
         <div className="todo-list">
             <h1>Todo List</h1>
-            <select onChange={(e) => setSortType(e.target.value)}>
+            <select className="sort-select" onChange={(e) => setSortType(e.target.value)}>
                 <option value="title">タイトルでソート</option>
                 <option value="date">日付でソート</option>
             </select>
-            <button onClick={handleReset}>テストデータ復活(デバッグ用)</button>
             <AddTodo onAddTodo={handleAddTodo} />
+            <button onClick={handleReset}>テストデータ復活(デバッグ用)</button>
             <ul>
                 {todos.map(todo => (
                     <li key={todo.id} className={todo.completed ? 'completed' : ''}>
